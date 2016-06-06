63a90c5ef2f68e3a2d43951e260bda96f90d597d
litengfei <litengfei@qiyi.com>
2016-06-06 13:33:08 +0800

    add tableView Cell add and delete

---
+++ UITableView/IOS_TableView/IOS table1 type/AddDeleteViewController.h
@@ -0,0 +1,18 @@
+//
+//  AddDeleteViewController.h
+//  IOS table1 type
+//
+//  Created by 李腾飞 on 16/6/6.
+//  Copyright © 2016年 李腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddDeleteViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *txtField;
+
+@property (nonatomic, strong) NSMutableArray *listTeams;
+
+
+@end
---
+++ UITableView/IOS_TableView/IOS table1 type/AddDeleteViewController.m
@@ -0,0 +1,221 @@
+//
+//  AddDeleteViewController.m
+//  IOS table1 type
+//
+//  Created by 李腾飞 on 16/6/6.
+//  Copyright © 2016年 李腾飞. All rights reserved.
+//
+// 解决textView遮挡问题
+// 总结一下：
+//    1 注册键盘通知，具体情况具体判断
+//    1.1 缺点， 大量UITextView的时候 注册和注销通知无法搞定
+//    2.TableView的相应事件
+//    3.避免遮挡键盘方法，并没有搞清楚
+//    https://segmentfault.com/q/1010000002641514
+//    http://blog.sina.com.cn/s/blog_4c707018010136mk.html
+//
+
+#import "AddDeleteViewController.h"
+
+@implementation AddDeleteViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //设置TableView 点击弹起
+//    self.tableView scrollToRowAtIndexPath:<#(nonnull NSIndexPath *)#> atScrollPosition:<#(UITableViewScrollPosition)#> animated:<#(BOOL)#>
+    
+    //设置导航栏
+    self.navigationItem.rightBarButtonItem =  self.editButtonItem;
+    self.navigationItem.title = @"单元格插入和删除";
+    
+    //设置单元格文本框
+    self.txtField.hidden = YES;
+    self.txtField.delegate = self;
+    
+    self.listTeams = [[NSMutableArray alloc] initWithObjects:@"黑龙江", @"吉林", @"辽宁", nil];
+    
+    
+    
+    // Observe keyboard hide and show notifications to resize the text view appropriately.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+
+#pragma mark - 注册键盘通知方法
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    return;
+    
+    NSDictionary *userInfo = [notification userInfo];
+    
+    // Get the origin of the keyboard when it's displayed.
+    NSValue* aValue = [userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
+    
+    // Get the top of the keyboard as the y coordinate of its origin in self's view's coordinate system. The bottom of the text view's frame should align with the top of the keyboard's final position.
+    CGRect keyboardRect = [aValue CGRectValue];
+    keyboardRect = [self.view convertRect:keyboardRect fromView:nil];
+    
+    CGFloat keyboardTop = keyboardRect.origin.y;
+    CGRect newTextViewFrame = self.view.bounds;
+    newTextViewFrame.size.height = keyboardTop - self.view.bounds.origin.y;
+    
+    // Get the duration of the animation.
+    NSValue *animationDurationValue = [userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration;
+    [animationDurationValue getValue:&animationDuration];
+    
+    // Animate the resize of the text view's frame in sync with the keyboard's appearance.
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:animationDuration];
+    
+    self.txtField.frame = newTextViewFrame;
+    
+    [UIView commitAnimations];
+}
+
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    
+    return;
+    
+    NSDictionary* userInfo = [notification userInfo];
+    
+    
+    NSValue *animationDurationValue = [userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration;
+    [animationDurationValue getValue:&animationDuration];
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:animationDuration];
+    
+    self.txtField.frame = self.view.bounds;
+    
+    [UIView commitAnimations];
+}
+
+- (void)viewDidUnload
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+    
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark --UITableViewDataSource 协议方法
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.listTeams count] + 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    BOOL b_addCell = (indexPath.row == self.listTeams.count);
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:CellIdentifier] ;
+    }
+    
+    if (!b_addCell) {
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.textLabel.text = [self.listTeams objectAtIndex:indexPath.row];
+    } else {
+        self.txtField.frame = CGRectMake(10,0,300,44);
+        self.txtField.borderStyle = UITextBorderStyleNone;
+        self.txtField.placeholder = @"Add...";
+        self.txtField.text = @"";
+        [cell.contentView addSubview:self.txtField];
+    }
+    
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSArray* indexPaths = [NSArray arrayWithObject:indexPath];
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        [self.listTeams removeObjectAtIndex: indexPath.row];
+        [self.tableView deleteRowsAtIndexPaths:indexPaths
+                              withRowAnimation:UITableViewRowAnimationFade];
+        
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        
+        [self.listTeams insertObject:self.txtField.text atIndex:[self.listTeams count]];
+        [self.tableView insertRowsAtIndexPaths:indexPaths
+                              withRowAnimation:UITableViewRowAnimationFade];
+        
+    }
+    [self.tableView reloadData];
+    
+}
+
+#pragma mark --UITableViewDelegate 协议方法
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView
+           editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == [self.listTeams count]) {
+        return UITableViewCellEditingStyleInsert;
+    } else {
+        return UITableViewCellEditingStyleDelete;
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == [self.listTeams count]) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+#pragma mark -- UIViewController生命周期方法，用于响应视图编辑状态变化
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    
+    [self.tableView setEditing:editing animated:YES];
+    if (editing) {
+        self.txtField.hidden = NO;
+    } else {
+        self.txtField.hidden = YES;
+    }
+}
+
+#pragma mark -- UITextFieldDelegate委托方法,关闭键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return  YES;
+}
+
+#pragma mark -- UITextFieldDelegate委托方法,避免键盘遮挡文本框
+- (void) textFieldDidBeginEditing:(UITextField *)textField {
+    UITableViewCell *cell = (UITableViewCell*)textField.superview.superview.superview;
+    [self.tableView setContentOffset:CGPointMake(0.0, cell.frame.origin.y) animated:YES];
+    
+}
+
+
+@end
